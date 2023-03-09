9fc70611a0a25d199f8c2f14431cbc86b8571371
ayuanye <Y824301@163.com>
2023-03-09 18:01:06 +0800

    新学内容

--- src/类/3.继承.ts
+++ src/类/3.继承.ts
@@ -21,7 +21,15 @@
      * Dog extends Animal
      *  -此时，Animal被称为父类，Dog被称为子类
      *  -使用继承后，子类将会拥有父类所有的方法和属性
+     *  -通过继承可以将多个类中共有的代码写在一个父类中，
+     *      这样只需要写一次即可让所有的子类都同时拥有父类的属性和方法
+     *      如果希望在子类中添加一些父类中没有的属性后方法直接加就行了
+     *  -如果在子类中添加了和父类相同的方法，则子类方法掉父类的方法
+     *      这种子类覆盖掉父类方法的形式，我们称为重写
      */
+
+    // 定义一个表示狗的类
+    // 使用Dog类继承Animal类
     // class Dog{
     class Dog extends Animal{
         // name: string;
@@ -35,9 +43,17 @@
         // sayHello(){
         //     console.log('汪汪汪汪汪！');
         // }
+
+        run(){
+            console.log(`${this.name}再跑~~`)
+        }
+        sayHello(){
+            console.log('汪汪汪。。。')
+        }
     }
 
     // 定义一个表示猫的类
+    // 使用Cat继承Animal类
     // class Cat{
     class Cat extends Animal{
         // name: string;
@@ -48,9 +64,9 @@
         //     this.age = age;
         // }
 
-        // sayHello(){
-        //     console.log('喵喵喵喵喵！');
-        // }
+        sayHello(){
+            console.log('喵喵喵喵喵！');
+        }
     }
 
     
